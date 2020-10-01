b706bf026c918005eb709aa63d5c52970b320c94
admin <admin@172.19.170.231>
2020-10-01 09:04:40 +0200

    MySQL commande et Lignecommande sans erreur

--- CPOA TD2/src/projet/dao/MYSQLDAO/MySQLCommandeDAO.java
+++ CPOA TD2/src/projet/dao/MYSQLDAO/MySQLCommandeDAO.java
@@ -9,12 +9,13 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import projet.dao.modele.CommandeDAO;
 import projet.menu.Connexion;
-import projet.metier.*;
+import projet.metier.Commande;
+import projet.metier.LigneCommande;
+import projet.metier.Produit;
 
 public class MySQLCommandeDAO {
-	
+
 	private static MySQLCommandeDAO instance;
 
 	private MySQLCommandeDAO() {
@@ -26,107 +27,115 @@ public class MySQLCommandeDAO {
 		}
 		return instance;
 	}
-		
-		public static void create(int id_commande,LocalDate datecommande,int id_client) {
-	        
-	        try {
-	            Connection laConnexion = Connexion.creeConnexion();
-	            Statement insertion = laConnexion.createStatement();
-	            
-	                PreparedStatement requete = laConnexion.prepareStatement("insert into Commande (id_commande, date_commande, id_client) values (?,?,?)");
-					requete.setInt(1, id_commande);
-					requete.setDate(2, java.sql.Date.valueOf(datecommande));
-					requete.setInt(3, id_client);
-					int resu = requete.executeUpdate();
-					System.out.println("Insï¿½ration faite.");
-					
-	        }catch (SQLException sqle) {
-						System.out.println("Problï¿½me insert " + sqle.getMessage());
-						 			   }
-	    
+
+	public static void create(int id_commande, LocalDate datecommande, int id_client) {
+
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			Statement insertion = laConnexion.createStatement();
+
+			PreparedStatement requete = laConnexion
+					.prepareStatement("insert into Commande (id_commande, date_commande, id_client) values (?,?,?)");
+			requete.setInt(1, id_commande);
+			requete.setDate(2, java.sql.Date.valueOf(datecommande));
+			requete.setInt(3, id_client);
+			int resu = requete.executeUpdate();
+			System.out.println("Insï¿½ration faite.");
+
+		} catch (SQLException sqle) {
+			System.out.println("Problï¿½me insert " + sqle.getMessage());
+		}
+
 	}
 
-		public static void Delete(int idcommande) throws SQLException {
-			try {
+	public static void Delete(int idcommande) throws SQLException {
+		try {
 			Connection laConnexion = Connexion.creeConnexion();
 			PreparedStatement requete = laConnexion.prepareStatement("delete from Commande where id_commande=?");
-					requete.setInt(1, idcommande);
-					int res = requete.executeUpdate();
-					System.out.println("Suppresion faite.");
-			}catch (SQLException sqle) {
-				System.out.println("Problï¿½me delete " + sqle.getMessage());
-									   }
-			}
-		
-		public static void update(int idcommande, LocalDate datecommande, int idclient) {
-	        
-	        try {
-	            Connection laConnexion = Connexion.creeConnexion();
-	            Statement modification = laConnexion.createStatement();
-	            
-	                PreparedStatement requete = laConnexion.prepareStatement("Update Commande set date_commande=?, id_client=? where id_commande=?");
-	                requete.setDate(1, java.sql.Date.valueOf(datecommande));
-					requete.setInt(2,idclient);	
-					requete.setInt(3, idcommande);
-					int resu = requete.executeUpdate();
-					System.out.println("Modification faite.");
-					
-	        }catch (SQLException sqle) {
-				System.out.println("Problï¿½me modification " + sqle.getMessage());
-			   }
-	    }
-		
-		@SuppressWarnings("unchecked")
-		public static ArrayList<Commande> Commande(){
-			@SuppressWarnings("rawtypes")
-			ArrayList<Commande> p= new ArrayList();
-			try {
-				Connection laConnexion = Connexion.creeConnexion();
-				PreparedStatement requete = laConnexion.prepareStatement("select * from Commande");
-					ResultSet res = requete.executeQuery();
-					
-					while (res.next()) {
-						p.add(new Commande (res.getInt(1), res.getDate(2).toLocalDate(), res.getInt(3), ligneCommande);
-					}
-					
-			}catch (SQLException sqle) {
-				System.out.println("Problï¿½me ArrayList " + sqle.getMessage());
-			   }
-			return p;
-			
+			requete.setInt(1, idcommande);
+			int res = requete.executeUpdate();
+			System.out.println("Suppresion faite.");
+		} catch (SQLException sqle) {
+			System.out.println("Problï¿½me delete " + sqle.getMessage());
 		}
-		
-		public Commande getById(int id) throws SQLException {
-			Commande commande = null;
-			
-			HashMap<Produit, LigneCommande> ligneCommande = new HashMap<Produit, LigneCommande>();
-			
+	}
+
+	public static void update(int idcommande, LocalDate datecommande, int idclient) {
+
+		try {
 			Connection laConnexion = Connexion.creeConnexion();
-			
-			//requete pour obtenir la commande desiree
-			PreparedStatement requete = laConnexion.prepareStatement("select * from `Commande` where id_commande=" + id);
-			ResultSet res = requete.executeQuery();
-			
-			if (res.next()) {
-				//requete pour obtenir toute les lignes de commandes concern�es par cette commande
-				PreparedStatement requeteLc = laConnexion.prepareStatement("select * from `Ligne_Commande` where id_commande=" + id);
-				ResultSet resLc = requeteLc.executeQuery();
-				
-				while (resLc.next()) {
-					//requete pour obtenir pour les produits concern�s par la commande
-					PreparedStatement requeteProd = laConnexion.prepareStatement("select * from `Produit` where id=" + resLc.getInt(2));
-					ResultSet resProd = requeteProd.executeQuery();
-					
-					ligneCommande.put(new Produit(resProd.getInt(1), resProd.getString(2), resProd.getString(3), resProd.getFloat(4), resProd.getString(5), resProd.getInt(6)), 
-						new LigneCommande(resLc.getInt(1), resLc.getInt(2), resLc.getInt(3), resLc.getFloat(4)));
-				}
-			
-				commande = new Commande(res.getInt(1), res.getDate(2).toLocalDate(), res.getInt(3), ligneCommande);
-			}
-			
-			if (laConnexion != null)
-				laConnexion.close();
-			
-			return commande;
+			Statement modification = laConnexion.createStatement();
+
+			PreparedStatement requete = laConnexion
+					.prepareStatement("Update Commande set date_commande=?, id_client=? where id_commande=?");
+			requete.setDate(1, java.sql.Date.valueOf(datecommande));
+			requete.setInt(2, idclient);
+			requete.setInt(3, idcommande);
+			int resu = requete.executeUpdate();
+			System.out.println("Modification faite.");
+
+		} catch (SQLException sqle) {
+			System.out.println("Problï¿½me modification " + sqle.getMessage());
 		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static ArrayList<Commande> Commande() {
+		@SuppressWarnings("rawtypes")
+		ArrayList<Commande> p = new ArrayList();
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			PreparedStatement requete = laConnexion.prepareStatement("select * from Commande");
+			ResultSet res = requete.executeQuery();
+
+			while (res.next()) {
+				HashMap<Produit, LigneCommande> ligneCommande = new HashMap<Produit, LigneCommande>();
+				p.add(new Commande(res.getInt(1), res.getDate(2).toLocalDate(), res.getInt(3), ligneCommande));
+			}
+
+		} catch (SQLException sqle) {
+			System.out.println("Problï¿½me ArrayList " + sqle.getMessage());
+		}
+		return p;
+
+	}
+
+	public Commande getById(int id) throws SQLException {
+		Commande commande = null;
+
+		HashMap<Produit, LigneCommande> ligneCommande = new HashMap<Produit, LigneCommande>();
+
+		Connection laConnexion = Connexion.creeConnexion();
+
+		// requete pour obtenir la commande desiree
+		PreparedStatement requete = laConnexion.prepareStatement("select * from `Commande` where id_commande=" + id);
+		ResultSet res = requete.executeQuery();
+
+		if (res.next()) {
+			// requete pour obtenir toute les lignes de commandes concern�es par cette
+			// commande
+			PreparedStatement requeteLc = laConnexion
+					.prepareStatement("select * from `Ligne_Commande` where id_commande=" + id);
+			ResultSet resLc = requeteLc.executeQuery();
+
+			while (resLc.next()) {
+				// requete pour obtenir pour les produits concern�s par la commande
+				PreparedStatement requeteProd = laConnexion
+						.prepareStatement("select * from `Produit` where id=" + resLc.getInt(2));
+				ResultSet resProd = requeteProd.executeQuery();
+
+				ligneCommande.put(
+						new Produit(resProd.getInt(1), resProd.getString(2), resProd.getString(3), resProd.getFloat(4),
+								resProd.getString(5), resProd.getInt(6)),
+						new LigneCommande(resLc.getInt(1), resLc.getInt(2), resLc.getInt(3), resLc.getFloat(4)));
+			}
+
+			commande = new Commande(res.getInt(1), res.getDate(2).toLocalDate(), res.getInt(3), ligneCommande);
+		}
+
+		if (laConnexion != null)
+			laConnexion.close();
+
+		return commande;
+	}
 }
--- CPOA TD2/src/projet/dao/MYSQLDAO/MySQLLigneCommandeDAO.java
+++ CPOA TD2/src/projet/dao/MYSQLDAO/MySQLLigneCommandeDAO.java
@@ -6,82 +6,86 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collection;
 
 import projet.menu.Connexion;
+import projet.metier.LigneCommande;
 
 public class MySQLLigneCommandeDAO {
-	
-public static void create(int idcommande, int idproduit, int quantite,float tarifunitaire) {
-        
-        try {
-            Connection laConnexion = Connexion.creeConnexion();
-            Statement insertion = laConnexion.createStatement();
-            
-                PreparedStatement requete = laConnexion.prepareStatement("insert into Ligne_commande (id_commande,id_produit,quantite,tarif_unitaire) values (?,?,?,?)");
-				requete.setInt(1, idcommande);
-				requete.setInt(2, idproduit);
-				requete.setInt(3, quantite);
-				requete.setFloat(4, tarifunitaire);
-				int resu = requete.executeUpdate();
-				System.out.println("Ins�ration faite.");
-				
-        }catch (SQLException sqle) {
-					System.out.println("Probl�me insert " + sqle.getMessage());
-					 			   }
-    
-}
 
-	public static void delete(int idcommande ,int idproduit) throws SQLException {
-		try {
-		Connection laConnexion = Connexion.creeConnexion();
-		PreparedStatement requete = laConnexion.prepareStatement("delete from Ligne_commande where id_commande=? and id_produit=?");
-				requete.setInt(1, idcommande);
-				requete.setInt(2, idproduit);
-				int res = requete.executeUpdate();
-				System.out.println("Suppresion faite.");
-		}catch (SQLException sqle) {
-			System.out.println("Probl�me delete " + sqle.getMessage());
-								   }
-		}
-	
-	public static void update(int idcommande, int idproduit,int quantite, float tarifunitaire) {
-        
-        try {
-            Connection laConnexion = Connexion.creeConnexion();
-            Statement modification = laConnexion.createStatement();
-            
-                PreparedStatement requete = laConnexion.prepareStatement("Update Produit set nom=?, description=?, tarif=?, visuel=?, id_categorie=? where id_produit=?");
-				requete.setString(1,nom);
-				requete.setString(2,description);	
-				requete.setFloat(3, tarif);
-				requete.setString(4, visuel);
-				requete.setInt(5, idcateg);	
-				requete.setInt(6, idproduit);
-				int resu = requete.executeUpdate();
-				System.out.println("Modification faite.");
-				
-        }catch (SQLException sqle) {
-			System.out.println("Probl�me modification " + sqle.getMessage());
-		   }
-    }
-	
-	@SuppressWarnings("unchecked")
-	public static ArrayList<MySQLProduitDAO> Produit(){
-		@SuppressWarnings("rawtypes")
-		ArrayList<MySQLProduitDAO> p= new ArrayList();
+	public static void create(int idcommande, int idproduit, int quantite, float tarifunitaire) {
+
 		try {
 			Connection laConnexion = Connexion.creeConnexion();
-			PreparedStatement requete = laConnexion.prepareStatement("select * from Produit");
-				ResultSet res = requete.executeQuery();
-				
-				while (res.next()) {
-					p.add(new Produit (res.getInt(1),res.getString(2),res.getString(3),res.getFloat(4),res.getString(5),res.getInt(6)));
-				}
-				
-		}catch (SQLException sqle) {
+			Statement insertion = laConnexion.createStatement();
+
+			PreparedStatement requete = laConnexion.prepareStatement(
+					"insert into Ligne_commande (id_commande,id_produit,quantite,tarif_unitaire) values (?,?,?,?)");
+			requete.setInt(1, idcommande);
+			requete.setInt(2, idproduit);
+			requete.setInt(3, quantite);
+			requete.setFloat(4, tarifunitaire);
+			int res = requete.executeUpdate();
+			System.out.println("Ins�ration faite.");
+
+		} catch (SQLException sqle) {
+			System.out.println("Probl�me insert " + sqle.getMessage());
+		}
+
+	}
+
+	public static void delete(int idcommande, int idproduit) throws SQLException {
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			PreparedStatement requete = laConnexion
+					.prepareStatement("delete from Ligne_commande where id_commande=? and id_produit=?");
+			requete.setInt(1, idcommande);
+			requete.setInt(2, idproduit);
+			int res = requete.executeUpdate();
+			System.out.println("Suppresion faite.");
+		} catch (SQLException sqle) {
+			System.out.println("Probl�me delete " + sqle.getMessage());
+		}
+	}
+
+	public static void update(int idcommande, int idproduit, int quantite, float tarifunitaire) {
+
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			Statement modification = laConnexion.createStatement();
+
+			PreparedStatement requete = laConnexion.prepareStatement(
+					"Update Ligne_commande set quantite=?, tarif_unitaire=? where id_commande=? and id_produit=?");
+			requete.setInt(1, quantite);
+			requete.setFloat(2, tarifunitaire);
+			requete.setInt(3, idcommande);
+			requete.setInt(4, idproduit);
+			int res = requete.executeUpdate();
+			System.out.println("Modification faite.");
+
+		} catch (SQLException sqle) {
+			System.out.println("Probl�me modification " + sqle.getMessage());
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static ArrayList<MySQLLigneCommandeDAO> LigneCommande() {
+		@SuppressWarnings("rawtypes")
+		ArrayList<MySQLLigneCommandeDAO> p = new ArrayList();
+		try {
+			Connection laConnexion = Connexion.creeConnexion();
+			PreparedStatement requete = laConnexion.prepareStatement("select * from Ligne_commande");
+			ResultSet res = requete.executeQuery();
+
+			while (res.next()) {
+				p.addAll((Collection<? extends MySQLLigneCommandeDAO>) new LigneCommande(res.getInt(1), res.getInt(2),
+						res.getInt(3), res.getFloat(4)));
+			}
+
+		} catch (SQLException sqle) {
 			System.out.println("Probl�me ArrayList " + sqle.getMessage());
-		   }
+		}
 		return p;
-		
+
 	}
 }
