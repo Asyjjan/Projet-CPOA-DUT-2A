fc20fbd9e44194f40477887bd1ce9c3cae8e7f25
admin <admin@192.168.1.19>
2020-10-08 08:45:17 +0200

    Test erreur booleen,Commande et LigneCommande

--- CPOA TD2/src/projet/dao/test/ListeMemoireLigneCommandeDAOTest.java
+++ CPOA TD2/src/projet/dao/test/ListeMemoireLigneCommandeDAOTest.java
@@ -6,58 +6,59 @@ import projet.dao.DAOFactory;
 import projet.dao.Persistance;
 import projet.metier.LigneCommande;
 
-public class MYSQLLigneCommandeDAOTest {
+public class ListeMemoireLigneCommandeDAOTest {
 
+	@Test
 	public void testCreateCasGeneral() {
-		LigneCommande c = new LigneCommande(9, 40);
-		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
+		LigneCommande c = new LigneCommande(7, 20);
+		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.LISTE_MEMOIRE);
 		assertTrue(daos.getLigneCommandeDAO().create(c));
 	}
 
 	@Test
 	public void testCreateExistant() {
 		LigneCommande c = new LigneCommande(1, 15);
-		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
+		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.LISTE_MEMOIRE);
 		assertFalse(daos.getLigneCommandeDAO().create(c));
 	}
 
 	@Test
 	public void testUpdateCasGeneral() {
-		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
+		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.LISTE_MEMOIRE);
 		LigneCommande c = (LigneCommande) daos.getLigneCommandeDAO().getById(1);
-		c.setTarif(40);
-		assertTrue(daos.getClientDAO().update(c));
+		c.setTarif(20);
+		assertTrue(daos.getLigneCommandeDAO().update(c));
 	}
 
 	@Test
 	public void testDeleteCasGeneral() {
-		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
-		LigneCommande c = (LigneCommande) daos.getLigneCommandeDAO().getById(9);
-		assertTrue(daos.getClientDAO().delete(c));
+		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.LISTE_MEMOIRE);
+		LigneCommande c = (LigneCommande) daos.getLigneCommandeDAO().getById(1);
+		assertTrue(daos.getLigneCommandeDAO().delete(c));
 	}
 
 	@Test
 	public void testDeleteInexistant() {
-		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
+		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.LISTE_MEMOIRE);
 		assertFalse(daos.getLigneCommandeDAO().delete(daos.getLigneCommandeDAO().getById(11)));
 	}
 
 	@Test
 	public void testUpdateInexistant() {
-		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
+		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.LISTE_MEMOIRE);
 		assertFalse(daos.getLigneCommandeDAO().update(daos.getLigneCommandeDAO().getById(11)));
 	}
 
 	@Test
 	public void getById() {
-		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
+		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.LISTE_MEMOIRE);
 		int i = ((LigneCommande) daos.getLigneCommandeDAO().getById(1)).getquantite();
 		assertEquals(i, 1);
 	}
 
 	@Test
 	public void getByIdInexistant() {
-		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.MYSQL);
+		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.LISTE_MEMOIRE);
 		assertEquals(daos.getLigneCommandeDAO().getById(12), null);
 	}
 }
