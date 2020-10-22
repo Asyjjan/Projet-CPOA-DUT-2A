788a652eade5dea179d9d481550f9c5f06c43816
Sylvio <Etudiant@sly-bu-p-msau18>
2020-10-22 12:33:16 +0200

    maj

--- CPOA TD2/src/projet/controleur/MainController.java
+++ CPOA TD2/src/projet/controleur/MainController.java
@@ -2,12 +2,15 @@ package projet.controleur;
 
 import java.io.IOException;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import projet.main.Main;
 
 public class MainController {
 
