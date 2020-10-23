17ca75e4d236a5980fd06ce43c8110640001368a
gisonni2u <gisonni2u@iutm-inf-e37-20.iutmetz.site.univ-lorraine.fr>
2020-10-23 10:22:24 +0200

    MAj client 2

--- CPOA TD2/src/projet/controleur/PageClientController.java
+++ CPOA TD2/src/projet/controleur/PageClientController.java
@@ -14,6 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import projet.metier.Client;
 
